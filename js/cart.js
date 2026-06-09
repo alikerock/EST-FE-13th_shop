969592907589645390413a88cd00f78ff5b6ace2
alikerock <alikerock@gmail.com>
2026-06-09 09:37:36 +0900

    feat : cartHTML 추가

--- js/cart.js
+++ js/cart.js
@@ -3,3 +3,28 @@ import { readCart, updateCartCount } from "./utils/common.js";
 updateCartCount();
 const cart = readCart();
 console.log(cart);
+
+const cartHTML = cart.map(
+  item =>
+    `<article class="cart-item">
+      <span class="item-check"><span class="check-box" aria-hidden="true"></span></span>
+      <div class="cart-thumb">
+        <img
+          src="${item.thumb}"
+          alt="${item.title}"
+        />
+      </div>
+      <div class="cart-item-info">
+        <h2>${item.title}</h2>
+        <p>브랜드명 | ${item.brand}</p>
+        <strong>189,000원</strong>
+      </div>
+      <div class="quantity-box" aria-label="수량">
+        <button type="button" aria-label="수량 줄이기">-</button>
+        <span>1</span>
+        <button type="button" aria-label="수량 늘리기">+</button>
+      </div>
+      <button type="button" class="remove-item" aria-label="${item.title} 삭제"></button>
+    </article>
+  `,
+);
